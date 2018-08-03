b06f664e46eff51e33fe5272a39b874b9a659f28
Mu Qiao <Mu.Qiao@zooplus.com>
2018-08-03 14:14:35 +0200

    changes

--- src/main/java/com/mywork/nashlalala/TestSonar.java
+++ src/main/java/com/mywork/nashlalala/TestSonar.java
@@ -2,6 +2,6 @@ package com.mywork.nashlalala;
 
 public class TestSonar {
     public static void main(String[] args) {
-        System.out.println("test sonar");
+        System.out.println("test sonar changes");
     }
 }
